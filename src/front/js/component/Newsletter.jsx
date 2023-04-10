1abbc09b146af7f880001e00347b4daa5030ad0a
Ibai Fernández <info@ibaifernandez.com>
2023-04-10 23:37:46 +0000

    APIed Mailerlite

--- src/front/js/component/Newsletter.jsx
+++ src/front/js/component/Newsletter.jsx
@@ -1,27 +1,63 @@
 import React, { useState, useEffect } from "react";
 
 export const Newsletter = () => {
+    const MLT = ""
+    
     const [usermail, setUsermail] = useState("");
+
+    const [successfulSubscription, setSuccessfulSubscription] = useState("");
+    const [subscriptionError, setSubscriptionError] = useState("");
 
     const handleUsermail = (e) => {
         setUsermail(e.target.value);
     };
 
     const addSubscriber = (e) => {
-        e.preventDefault()
-        // fetch(`https://api.mailerlite.com/api/v2/groups/${groupId}/subscribers`, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'X-MailerLite-ApiKey': apiKey,
-        //     },
-        //     body: JSON.stringify({"email": usermail}),
-        // })
-        // .then(response => response.json())
-        // .then(data => console.log(data))
-        // .catch(error => console.error(error));
-}
-   
+        e.preventDefault();
+
+        if (usermail === "") {
+            setSuccessfulSubscription(false);
+            setSubscriptionError("Por favor, ingresa tu correo electrónico.");
+            return;
+        }
+
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+            if (!emailRegex.test(usermail)) {
+            setSuccessfulSubscription(false);
+            setSubscriptionError("Por favor, ingresa una dirección de correo electrónico válida.");
+            return;
+        }
+
+        fetch("https://connect.mailerlite.com/api/subscribers", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+             Authorization: "Bearer " + MLT,
+            },
+            body: JSON.stringify({
+                email: usermail,
+              fields: { name: "xxx" },
+                groups: ["85110772424771310"],
+            }),
+          })
+            .then((response) => {
+                if (response.status === 200) {
+                setSuccessfulSubscription("Suscrito con éxito. ¡Gracias!");
+                setSubscriptionError("");
+                } else {
+               setSuccessfulSubscription(false);
+                setSubscriptionError("Ocurrió un error al intentar suscribirse. Por favor, inténtelo de nuevo más tarde.");
+                }
+                return response.json();
+            })
+            .catch((error) => {
+                console.error(error);
+                setSuccessfulSubscription(false);
+                setSubscriptionError("Ocurrió un error al intentar suscribirse. Por favor, inténtelo de nuevo más tarde.");
+            });
+        };
+      
     return (
         <div className="footer-newsletter">
             <div className="container">
@@ -43,8 +79,10 @@ export const Newsletter = () => {
                                 onChange={handleUsermail}
                                 placeholder="Escribe aquí tu correo electrónico"
                             />
-                            <button id="newsletter-btn" value="Suscríbete" onClick={addSubscriber}>Adjúntate</button>
+                            <button id="newsletter-btn" value="Suscríbete" onClick={addSubscriber}>Suscríbete</button>
                         </form>
+                        {subscriptionError ? <p className="mt-3">{subscriptionError}</p> : null}
+                        {successfulSubscription !== "" ? <p className="mt-3">{successfulSubscription}</p> : null}
                     </div>
                 </div>
             </div>
