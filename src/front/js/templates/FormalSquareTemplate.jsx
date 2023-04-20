a87d2b6a206dfaf4f996c21c6ac6668b7dd75778
Sollb77 <qsiecuadorservicios@gmail.com>
2023-04-20 22:20:09 +0000

    mod

--- src/front/js/templates/FormalSquareTemplate.jsx
+++ src/front/js/templates/FormalSquareTemplate.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 
 import "../../styles/templates/formal-square-template.css";
@@ -7,25 +7,33 @@ import { Context } from "../store/appContext";
 
 
 export const FormalSquareTemplate = () => {
- 
-      const { store, actions } = useContext(Context);
+   
+  const { store, actions } = useContext(Context)
+
+  useEffect(()=>{
+    actions.getInfoPost()  
+},[])
+
+const { auxColor, contactData, identity, mainColor, mainText, price, secondaryColor, secondaryText, cta, logo } = store.infoPost
+
+
       return (
       <>
-      <div className="container-fluid CuadroQueContiene">
-        <div className="CuadradoExterior">
-          <img className="logoPFF" src={store.infoPost.logo} alt=""/>
+      <div className="container-fluid CuadroQueContiene" >
+        <div className="CuadradoExterior" id="vsft">
+          <img className="logoPFF" src={localStorage.logo} alt=""/>
           <div className="border-bottom divTextoPrincipal">
-            <h3  className="TextoPrincipal text-decoration-underline">{store.infoPost.main_text}</h3>
+            <h3  className="TextoPrincipal text-uppercase font-weight-bold">{mainText}</h3>
           </div>
-          <h5 className="TextoSecundario">{store.infoPost.secondary_text}</h5>
-          <div className="rounded-circle circulo overflow-hidden" style={{backgroundColor:"{store.infoPost.main_color}"}}></div>
-          <div className="CuadradoBorde border border-5" style={{backgroundColor:"{store.infoPost.aux_color}"}}></div>
-          <div className="CuadradoColor2" style={{backgroundColor:"{store.infoPost.secondary_color}"}}></div>
-          <img src="https://cnnespanol.cnn.com/wp-content/uploads/2023/02/230217101303-deportes-diana-flores-football-full-169.jpg?quality=100&strip=info&w=780&h=438&crop=1" alt="" className="ImagenCargada shadow-lg p-3 mb-5 bg-body-tertiary rounded"/>
-          <div className="oferta p-2"><h4>{store.infoPost.price}</h4></div>
-          <h3 className="TextoPrincipalb">{store.infoPost.identity}</h3>
-          <div className="Cuadricula1" style={{backgroundColor:"{store.infoPost.secondary_color}"}}></div>
-          <div className="Cuadricula2" style={{backgroundColor:"{store.infoPost.secondary_color}"}}></div>
+          <h5 className="TextoSecundario">{secondaryText}</h5>
+          <div className="rounded-circle circulo overflow-hidden" style={{backgroundColor:`${mainColor}`}}></div>
+          <div className="CuadradoBorde border border-5" style={{borderColor:`${auxColor}`}}></div>
+          <div className="CuadradoColor2" style={{backgroundColor:`${secondaryColor}`}}></div>
+          <img src={localStorage.mainImage} alt="" className="ImagenCargada shadow-lg p-3 mb-5 bg-body-tertiary rounded"style={{borderColor:`${auxColor}`}}/>
+          <div className="oferta p-2 text-center text-light"><p>{price}</p></div>
+          <h3 className="TextoPrincipalb text-uppercase font-weight-bold">{identity}</h3>
+          <div className="Cuadricula1" style={{backgroundColor:`${auxColor}`}}></div>
+          <div className="Cuadricula2" style={{backgroundColor:`${auxColor}`}}></div>
         </div>
         <div className="recorte1"></div>
         <div className="recorte2"></div>
--- src/front/js/templates/FormalVerticalTemplate.jsx
+++ src/front/js/templates/FormalVerticalTemplate.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "../../styles/templates/formal-vertical-template.css";
 import { Context } from "../store/appContext";
 
@@ -6,26 +6,34 @@ import { Context } from "../store/appContext";
 
 export const FormalVerticalTemplate = () => {
   
-  const { store } = useContext(Context);
+  const { store, actions } = useContext(Context);
   
+  
+  useEffect(()=>{
+    actions.getInfoPost()  
+},[])
+
+const { auxColor, contactData, identity, mainColor, mainText, price, secondaryColor, secondaryText, cta, logo } = store.infoPost
+
+
   return (
   <>  
-    <div className="container-fluid CuadroQueContieneS">
+    <div className="container-fluid CuadroQueContieneS" id="vsft">
       <div className=" CuadradoBlancoS">  
         <div className="CuadradoExteriorS">
-        <img className="logoPFFS" src={store.infoPost.logo} alt=""/>
+        <img className="logoPFFS" src={localStorage.logo} alt=""/>
         <div className="border-bottom divTextoPrincipalS">
-        <h3  className="TextoPrincipalS text-decoration-underline">{store.infoPost.main_text}"Texto Principal"</h3>
+        <h3  className="TextoPrincipalS text-decoration-underline">{mainText}</h3>
         </div>
-        <h5 className="TextoSecundarioS">{store.infoPost.secondary_text}"Texto Secundario"</h5>
-        <div className="rounded-circle circuloS overflow-hidden" style={{backgroundColor:"{store.infoPost.main_color}"}}></div>
-        <div className="CuadradoBordeS border border-5" style={{backgroundColor:"{store.infoPost.aux_color}"}}></div>
-        <div className="CuadradoColor2S" style={{backgroundColor:"{store.infoPost.secondary_color}"}}></div>
-        <img src="https://cnnespanol.cnn.com/wp-content/uploads/2023/02/230217101303-deportes-diana-flores-football-full-169.jpg?quality=100&strip=info&w=780&h=438&crop=1" alt="" className="ImagenCargadaS shadow-lg p-3 mb-5 bg-body-tertiary rounded"/>
-        <div className="ofertaS p-2 border border-light-subtle ps-5 pt-3"><h4>{store.infoPost.price}"Oferta"</h4></div>
-        <h3 className="TextoPrincipalbS">{store.infoPost.identity} "Identidad"</h3>
-        <div className="Cuadricula1S" style={{backgroundColor:"{store.infoPost.secondary_color}"}}></div>
-        <div className="Cuadricula2S" style={{backgroundColor:"{store.infoPost.secondary_color}"}}></div>
+        <h5 className="TextoSecundarioS">{secondaryText}</h5>
+        <div className="rounded-circle circuloS overflow-hidden" style={{backgroundColor:`${mainColor}`}}></div>
+        <div className="CuadradoBordeS border border-5" style={{backgroundColor:`${auxColor}`}}></div>
+        <div className="CuadradoColor2S" style={{backgroundColor:`${secondaryColor}`}}></div>
+        <img src={localStorage.mainImage} alt="" className="ImagenCargadaS shadow-lg p-3 mb-5 bg-body-tertiary rounded"/>
+        <div className="ofertaS p-2 border border-light-subtle ps-5 pt-3"><h4>{price}</h4></div>
+        <h3 className="TextoPrincipalbS">{identity} </h3>
+        <div className="Cuadricula1S" style={{backgroundColor:`${secondaryColor}`}}></div>
+        <div className="Cuadricula2S" style={{backgroundColor:`${secondaryColor}`}}></div>
     </div>
         <div className="recorte1S"></div>
         <div  className="recorte2S"></div>
