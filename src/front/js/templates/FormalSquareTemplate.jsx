fea7b0ee68d13d730909c28b42ec7085935f8683
Sollb77 <qsiecuadorservicios@gmail.com>
2023-04-21 16:16:06 +0000

    ¨mod5¨

--- src/front/js/templates/FormalSquareTemplate.jsx
+++ src/front/js/templates/FormalSquareTemplate.jsx
@@ -1,6 +1,4 @@
 import React, { useContext, useEffect } from "react";
-
-
 import "../../styles/templates/formal-square-template.css";
 import { Context } from "../store/appContext";
 
@@ -31,8 +29,8 @@ const { auxColor, contactData, identity, mainColor, mainText, price, secondaryCo
           <div className="CuadradoColor2" style={{backgroundColor:`${secondaryColor}`}}></div>
           <img src={localStorage.mainImage} alt="" className="ImagenCargada shadow-lg p-3 mb-5 bg-body-tertiary rounded"style={{borderColor:`${auxColor}`}}/>
           <div className="oferta p-2 text-center text-light"><p>{price}</p></div>
-          <h3 className="TextoPrincipalb text-uppercase font-weight-bold text-right">{identity}</h3>
-          <h3 className="ContactData text-uppercase font-weight-bold text-right">{contactData}</h3>
+          <h3 className="TextoPrincipalb text-uppercase font-weight-bold text-right fw-bold align-bottom">{identity}</h3>
+          <h3 className="ContactData text-uppercase font-weight-bold text-right ">{contactData}</h3>
           <div className="Cuadricula1" style={{backgroundColor:`${auxColor}`}}></div>
           <div className="Cuadricula2" style={{backgroundColor:`${auxColor}`}}></div>
         </div>
--- src/front/js/templates/FormalVerticalTemplate.jsx
+++ src/front/js/templates/FormalVerticalTemplate.jsx
@@ -19,22 +19,23 @@ const { auxColor, contactData, identity, mainColor, mainText, price, secondaryCo
   return (
   <>  
     <div className="container-fluid CuadroQueContieneS" id="vsft">
-      <div className=" CuadradoBlancoS">  
+       
         <div className="CuadradoExteriorS">
         <img className="logoPFFS" src={localStorage.logo} alt=""/>
         <div className="border-bottom divTextoPrincipalS">
-        <h3  className="TextoPrincipalS text-decoration-underline">{mainText}</h3>
+        <h3  className="TextoPrincipalS text-uppercase font-weight-bold fs-4">{mainText}</h3>
         </div>
-        <h5 className="TextoSecundarioS">{secondaryText}</h5>
+        <h5 className="TextoSecundarioS fs-5">{secondaryText}</h5>
         <div className="rounded-circle circuloS overflow-hidden" style={{backgroundColor:`${mainColor}`}}></div>
-        <div className="CuadradoBordeS border border-5" style={{backgroundColor:`${auxColor}`}}></div>
+        <div className="CuadradoBordeS border border-5" style={{borderColor:`${auxColor}`}}></div>
         <div className="CuadradoColor2S" style={{backgroundColor:`${secondaryColor}`}}></div>
-        <img src={localStorage.mainImage} alt="" className="ImagenCargadaS shadow-lg p-3 mb-5 bg-body-tertiary rounded"/>
-        <div className="ofertaS p-2 border border-light-subtle ps-5 pt-3"><h4>{price}</h4></div>
-        <h3 className="TextoPrincipalbS">{identity} </h3>
+        <img src={localStorage.mainImage} alt="" className="ImagenCargadaS shadow-lg p-3 mb-5 bg-body-tertiary rounded" style={{borderColor:`${auxColor}`}}/>
+        <div className="ofertaS p-2 border border-light-subtle text-center text-light"><p>{price}</p></div>
+        <h3 className="TextoPrincipalbS text-uppercase font-weight-bold text-right fs-5">{identity} </h3>
+        <h3 className="ContactDataS text-uppercase font-weight-bold text-right">{contactData}</h3>
         <div className="Cuadricula1S" style={{backgroundColor:`${secondaryColor}`}}></div>
         <div className="Cuadricula2S" style={{backgroundColor:`${secondaryColor}`}}></div>
-    </div>
+    
         <div className="recorte1S"></div>
         <div  className="recorte2S"></div>
     </div>
